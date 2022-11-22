77a08b468db56ef7f20827b641412626bc255067
VoronovV <vitaliyvoronov2001@gmail.com>
2022-11-22 22:18:54 +0300

    add entry and registration pages

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
+from GUI import *
+import sys
+
+class setup(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(setup, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.main_page) #устанавливает главную страницу
+        self.init_UI()
+
+    def init_UI(self):
+
+        self.ui.entry_btn.clicked.connect(self.on_click_entry_but)
+        self.ui.regisration_btn.clicked.connect(self.on_click_registration_but)
+
+
+        # self.__MainWindow = QtWidgets.QMainWindow()
+        # self.__ui = Ui_MainWindow
+        # self.__ui.setupUi(self.__MainWindow)
+        # self.__MainWindow.show()
+        # self.__ui.entry.clicked.connect(self.on_click_entry_but)
+        # self.__ui.regisration.clicked.connect(self.on_click_registration_but)
+        # sys.exit(app.exec_())  # завершение программы
+
+    def on_click_entry_but(self):
+        self.ui.main_page.hide()
+        self.ui.entry_page.show()
 
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def on_click_registration_but(self):
+        self.ui.main_page.hide()
+        self.ui.registration_page.show()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    application = setup()
+    application.show()
+    sys.exit(app.exec_())
