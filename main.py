5ee808aced0ffc626e74fce320cfb7fff858bba7
VoronovV <vitaliyvoronov2001@gmail.com>
2022-11-30 01:15:28 +0300

    add all tables

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from GUI import *
 from database import *
+
 import sys
+
 
 class Start(QtWidgets.QMainWindow):
     def __init__(self):
@@ -26,9 +28,8 @@ class Start(QtWidgets.QMainWindow):
         login = self.ui.entry_login_edit.text()  # сверить с бд
         password = self.ui.entry_pass_edit.text()
         if login_in_users_table(self.con, login, password) == True:
-            self.ui.entry_page.hide()
-            self.ui.table_page.show()
-            self.ui.back_btn_table.clicked.connect(self.back_in_main_page)
+            self.open_menu()
+
     def on_click_registration_but(self):
         self.ui.main_page.hide()
         self.ui.registration_page.show()
@@ -41,16 +42,57 @@ class Start(QtWidgets.QMainWindow):
         password_2 = self.ui.reg_pass_edit_2.text()
         if password == password_2:
             add_in_users_table(self.con, login, password)
-            self.ui.registration_page.hide()
-            self.ui.table_page.show()
-            self.ui.back_btn_table.clicked.connect(self.back_in_main_page)
+            self.open_menu()
 
 
     def back_in_main_page(self):
         self.ui.registration_page.hide()
         self.ui.entry_page.hide()
-        self.ui.table_page.hide()
+        self.ui.table_page_empl.hide()
+        self.ui.table_page_proc.hide()
+        self.ui.table_page_rooms.hide()
+        self.ui.table_page_clients.hide()
+        self.ui.menu_page.hide()
         self.ui.main_page.show()
+
+
+    def back_in_menu(self):
+        self.ui.table_page_empl.hide()
+        self.ui.table_page_proc.hide()
+        self.ui.table_page_rooms.hide()
+        self.ui.table_page_clients.hide()
+        self.ui.menu_page.show()
+
+    def open_menu(self):
+        self.ui.entry_page.hide()
+        self.ui.menu_page.show()
+        self.ui.exit_btn.clicked.connect(self.back_in_main_page)
+        self.ui.clients_btn.clicked.connect(self.open_clients_table)
+        self.ui.employees_btn.clicked.connect(self.open_empl_table)
+        self.ui.rooms_btn.clicked.connect(self.open_rooms_table)
+        self.ui.proc_btn.clicked.connect(self.open_proc_table)
+
+
+    def open_clients_table(self):
+        self.ui.menu_page.hide()
+        self.ui.table_page_clients.show()
+
+        self.ui.back_btn_clients.clicked.connect(self.back_in_menu)
+    def open_empl_table(self):
+        self.ui.menu_page.hide()
+        self.ui.table_page_empl.show()
+
+        self.ui.back_btn_empl.clicked.connect(self.back_in_menu)
+    def open_rooms_table(self):
+        self.ui.menu_page.hide()
+        self.ui.table_page_rooms.show()
+
+        self.ui.back_btn_rooms.clicked.connect(self.back_in_menu)
+    def open_proc_table(self):
+        self.ui.menu_page.hide()
+        self.ui.table_page_proc.show()
+
+        self.ui.back_btn_proc.clicked.connect(self.back_in_menu)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
